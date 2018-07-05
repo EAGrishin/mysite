15513dacb6ec68fe857fc8c22426e062444406d0
evgeny <eagrishin85@gmail.com>
2018-07-06 06:20:41 +0700

    costa-rico

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=mysite',
+    'username' => 'evgeny',
+    'password' => '1234567',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
